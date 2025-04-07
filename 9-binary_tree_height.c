99f2b8fd0d3b5d17d3504e2932e6249ea59576a6
ab10680 <10680@holbertonstudents.com>
2025-04-07 15:34:56 +0000

    task 9 - update 2

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t left_height;  /* Declare variables at the beginning */
+size_t right_height; /* Declare variables at the beginning */
 /* Check if the tree is NULL */
 if (tree == NULL)
 return (0);
 /* Calculate the height of the left and right subtrees */
-size_t left_height = binary_tree_height(tree->left);
-size_t right_height = binary_tree_height(tree->right);
-/* Return the greater height plus one for the edges */
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+/* Return the greater height */
 return ((left_height > right_height ? left_height : right_height));
 }
